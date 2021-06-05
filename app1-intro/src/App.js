f5943711b20e27fee56557382fa0fa2d051c8780
Rıdvan <ridvanabay@gmail.com>
2021-06-05 16:10:40 +0300

    132.Bölüm 358.Ders ReactStrap İle Çalışmak

--- app1-intro/src/App.js
+++ app1-intro/src/App.js
@@ -1,3 +1,4 @@
+import { Col, Container, Row } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
@@ -6,9 +7,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Container>
+      <Row>
       <Navi/>
+      </Row>
+      <Row>
+      <Col xs="3">
       <CategoryList/>
+      </Col>
+      <Col xs="3">
       <ProductList/>
+        </Col>
+      </Row>
+      </Container>
       </header>
     </div>
   );
