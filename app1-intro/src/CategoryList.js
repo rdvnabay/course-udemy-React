ac1f4eab712368404115d2fef27ffe8fb50bba05
Rıdvan <ridvanabay@gmail.com>
2021-06-05 17:12:20 +0300

    State İle Çalışmaya Başlamak 134.Bölüm - 361.Ders

--- app1-intro/src/CategoryList.js
+++ app1-intro/src/CategoryList.js
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from 'reactstrap';
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   //eski react sürümlerinde tanımlı olması gereken constructor yapısı
   // constructor(props){
   //   super(props)
-  //   state:{}
+  //   this.state:{}
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Telephone" },
+      { categoryId: 2, categoryName: "Telephone" },
+    ],
+  };
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map(category => (
+            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
