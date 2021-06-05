a497559c0c3a1b2907b250555ab77c19f4ab4d87
Rıdvan <ridvanabay@gmail.com>
2021-06-05 17:47:14 +0300

    Eventlerle Çalışmaya Başlamak 135.Bölüm 362.Ders

--- app1-intro/src/CategoryList.js
+++ app1-intro/src/CategoryList.js
@@ -9,18 +9,27 @@ export default class CategoryList extends Component {
   state = {
     categories: [
       { categoryId: 1, categoryName: "Telephone" },
-      { categoryId: 2, categoryName: "Telephone" },
+      { categoryId: 2, categoryName: "Computer" },
     ],
+    currentCategory: "",
   };
+  changeCategory=(category)=>{
+    this.setState({ currentCategory: category.categoryName })
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          {this.state.categories.map(category => (
-            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem
+              key={category.categoryId}
+              onClick={() =>this.changeCategory(category)}>
+              {category.categoryName}
+            </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
