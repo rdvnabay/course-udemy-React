c164947b8f9b41bbeb32956169d57cda11187d56
Rıdvan <ridvanabay@gmail.com>
2021-06-06 18:13:49 +0300

    Çoklu Form Elemanlarıyla Çalışmak 142.Bölüm 378.Ders

--- app1-intro/src/Form1Demo.js
+++ app1-intro/src/Form1Demo.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 
 export default class Form1Demo extends Component {
-  state = { userName: "" };
+  state = { userName: "", city:"" };
   onChangeHandler = (event) => {
-    this.setState({ userName: event.target.value });
+    //this.setState({ userName: event.target.value });
+    let name=event.target.name;
+    let value=event.target.value;
+    this.setState({[name]:value});
   };
   onSubmitHandler = (event) => {
     event.preventDefault();
@@ -14,8 +17,12 @@ export default class Form1Demo extends Component {
       <div>
         <form onSubmit={this.onSubmitHandler}>
           <h3>Username</h3>
-          <input type="text" onChange={this.onChangeHandler}></input>
+          <input name="userName" type="text" onChange={this.onChangeHandler}></input>
           <h3>{this.state.userName}</h3>
+
+          <h3>City</h3>
+          <input name="city" type="text" onChange={this.onChangeHandler}></input>
+          <h3>{this.state.city}</h3>
           <input type="submit" value="Save"></input>
         </form>
       </div>
