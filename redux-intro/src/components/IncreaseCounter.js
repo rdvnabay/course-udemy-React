03c61f20039b7de75c5eaf30bef9d4aec75f7d92
Rıdvan <ridvanabay@gmail.com>
2021-06-07 16:02:22 +0300

    Dispatch İle Çalışmak 143.Bölüm 386.Ders

--- redux-intro/src/components/IncreaseCounter.js
+++ redux-intro/src/components/IncreaseCounter.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {increaseCounter} from '../redux/actions/counterActions'
 
-export default class IncreaseCounter extends Component {
+class IncreaseCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button onClick={e =>{
+                    this.props.dispatch(increaseCounter())
+                }}>
+                1 Arttır
+                </button>
             </div>
         )
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return {action:bindActionCreators(IncreaseCounter,dispatch)}
+}
+
+export default connect(mapDispatchToProps)(IncreaseCounter)
