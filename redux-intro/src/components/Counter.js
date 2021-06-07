11ab6db849b595697f22c451f6fd640cc7f3b7dd
Rıdvan <ridvanabay@gmail.com>
2021-06-07 15:49:42 +0300

    Componentin Reducera Bağlanması 143.Bölüm 385.Ders

---
+++ redux-intro/src/components/Counter.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{this.props.counter}</h3>
+            </div>
+        )
+    }
+}
+function mapStateToProps(state){
+    return {counter:state.counterReducer}
+}
+
+export default connect(mapStateToProps)(Counter)
